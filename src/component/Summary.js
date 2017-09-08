a647c7794d9050ea289e15aac112d4d1acd63a05
Yuwei Zhou <yuwzho@microsoft.com>
2017-09-08 21:04:09 +0800

    add summary page

--- src/component/Summary.js
+++ src/component/Summary.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Summary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      summary: this.getSummary()
+    };
+
+    this.getSummary = this.getSummary.bind(this);
+    this.getListStyle = this.getListStyle.bind(this);
+  }
+
+  getSummary() {
+    return [
+      {
+        name: 'Jess',
+        finance: -100
+      },
+      {
+        name: 'An B',
+        finance: 0
+      },
+      {
+        name: 'Me',
+        finance: 299
+      }
+    ];
+  }
+
+  getListStyle(number) {
+    if (number === 0) {
+      return '';
+    } else if (number > 0) {
+      return 'success';
+    } else {
+      return 'danger';
+    }
+  }
+
   render() {
+    const { summary } = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <div className="summary container">
+        <h3>账单清算</h3>
+        <p>按照汇率blablabla</p>
+        <br/>
+        <br/>
+        <ListGroup>
+          {
+            summary.map(function (item, i) {
+              return (<ListGroupItem bsStyle={this.getListStyle(item.finance)}><strong>{item.name}</strong>&nbsp;&nbsp;&nbsp;{item.finance > 0 ? '应收' : '应支出'}{Math.abs(item.finance)}元人民币</ListGroupItem>);
+            }.bind(this))
+          }
+        </ListGroup>
       </div>
     );
   }
